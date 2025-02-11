ec05c54274c8d5667ea10c04804478de6ad7318e
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-02-11 21:29:35 +0200

    csc

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -27,7 +27,6 @@ class PostResource extends JsonResource
             "adminImage" => $this->admin->image,
             "image" => $this->image,
             "created_at" => $this->created_at,
-            // "postComments" => $this->postComments->where('status', 'active'),
             'postComments' => PostcommentActiveResource::collection($this->postComments->where('status', 'active')),
 
             'id' => $this->id,
