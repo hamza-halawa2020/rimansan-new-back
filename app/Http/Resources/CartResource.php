8e5b9e0e38cc8df443877a814f32dfc271f9b8b0
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-02-18 17:28:27 +0200

    hghg

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -14,6 +14,13 @@ class CartResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'user_id' => $this->user_id,
+            'quantity' => $this->quantity,
+            'created_at' => $this->created_at,
+        ];
     }
 }
