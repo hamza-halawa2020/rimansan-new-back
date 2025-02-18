759dcae39b20e5c043814ade6cf3a1608d183e2a
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-02-18 21:42:38 +0200

    sdsd

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::apiResource('clients', ClientController::class);
 
 /**************************** start coupon api ****************************/
 Route::apiResource('coupons', CouponController::class);
-Route::get('showCoupon', [CouponController::class, 'showCoupon']);
+Route::post('showCoupon', [CouponController::class, 'showCoupon']);
 /**************************** end coupon api ****************************/
 
 /**************************** start category api ****************************/
