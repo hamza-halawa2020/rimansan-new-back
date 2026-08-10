1addd0883d53c065524233e4519b1e1524d77c4f
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-08-10 11:36:47 +0300

    up

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Rimansan API is running',
+    ]);
 });
-
 
